39b549a06e40c24f15e8749f0672fc73eb39d52b
ndesai <ndesai@newrelic.com>
2025-05-08 16:10:05 -0500

    chore: update native agents for latest version

--- NewRelic.MAUI.Plugin/Platforms/Android/NewRelicMethodsImplementation.cs
+++ NewRelic.MAUI.Plugin/Platforms/Android/NewRelicMethodsImplementation.cs
@@ -126,7 +126,7 @@ public sealed class NewRelicMethodsImplementation : INewRelicMethods
 
 
         var newRelic = NRAndroidAgent.WithApplicationToken(applicationToken)
-            .WithApplicationFramework(Com.Newrelic.Agent.Android.ApplicationFramework.Maui, "1.1.9")
+            .WithApplicationFramework(Com.Newrelic.Agent.Android.ApplicationFramework.Maui, "1.1.10")
             .WithLoggingEnabled(agentConfig.loggingEnabled)
             .WithLogLevel(logLevelDict[agentConfig.logLevel]);
 
